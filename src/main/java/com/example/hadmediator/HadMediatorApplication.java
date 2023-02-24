ff8dd82f0731eec4a9139c3cf4dffee85f5714b3
rohitraman <rohitraman9944@gmail.com>
2023-02-24 23:58:22 +0530

    Initial commit

---
+++ src/main/java/com/example/hadmediator/HadMediatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.hadmediator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadMediatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadMediatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hadmediator/HadMediatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hadmediator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadMediatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
