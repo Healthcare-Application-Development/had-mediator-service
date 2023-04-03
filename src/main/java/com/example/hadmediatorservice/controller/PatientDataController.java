c9a89d4d6f2bc6ab83549b9437122cb929029458
Prithviraj Naik <prithvirajnaik17@gmail.com>
2023-04-03 20:01:05 +0530

    Updated PatientDataController.java

--- src/main/java/com/example/hadmediatorservice/controller/PatientDataController.java
+++ src/main/java/com/example/hadmediatorservice/controller/PatientDataController.java
@@ -6,10 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import com.example.hadmediatorservice.bean.PatientData;
-//import spring.restapi.model.PatientRequest;
 import com.example.hadmediatorservice.bean.Response;
 import com.example.hadmediatorservice.repository.PatientDataRepository;
-//import spring.restapi.repository.PatientDataRequestRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,7 +34,7 @@ public class PatientDataController {
     public Response getAllPatientData() throws ResourceNotFoundException{
         ArrayList<String> portNumbers = new ArrayList<String>();
         portNumbers.add("http://localhost:3002/api/receptionist/getAll");
-//        portNumbers.add("http://localhost:3000/api/receptionist/getAll");
+        portNumbers.add("http://localhost:3000/api/receptionist/getAll");
         List<Response> responseList=new ArrayList<>();
         for(int i = 0; i < portNumbers.size(); i++) {
             RestTemplate restTemplate = new RestTemplate();
